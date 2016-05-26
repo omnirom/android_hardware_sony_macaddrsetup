549f7d8c83c58ca8bee16f5ff2c7dbf7fe4a0dbd
Humberto Borba <humberos@gmail.com>
2016-05-26 17:22:33 -0300

    hardware: sony: macaddrsetup: Prevent build errors
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I655c9230eea18be8b16b177f535197352a0fe857

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(SONY_AOSP), true)
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -14,3 +16,5 @@ LOCAL_MODULE := macaddrsetup
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
+
+endif
